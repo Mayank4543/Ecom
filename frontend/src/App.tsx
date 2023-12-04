a99db204a2fafef89643ce8d218cd1ca0af55907
= <=>
2023-12-05 01:17:34 +0530

    creating types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
+import { sampleprodduct } from "./data"
 
 
 function App() {
 
 
   return (
-   
+    <>
+      <header>
+        <main>
+          {sampleprodduct.map((product) => (
+            <ul>
+              <li key={product.slug}>
+                {product.name}
+              </li>
+            </ul>
+          ))}
+        </main>
+      </header>
+    </>
   )
 }
 
---
+++ frontend/src/data.ts
@@ -0,0 +1,41 @@
+import { Product } from "./types/Product";
+
+export const sampleprodduct: Product[] = [
+    {
+        name: "mayank",
+        slug: "mayank",
+        categories: "Pants",
+        image: '../images/mayank.jpg',
+        price: 78,
+        countInStock: 15,
+        brand: "nike",
+        rating: 4.5,
+        numReviews: 14,
+        descritption: "high quality product"
+    },
+    {
+        name: "mayank",
+        slug: "mayank",
+        categories: "Pants",
+        image: '../images/mayank.jpg',
+        price: 78,
+        countInStock: 15,
+        brand: "nike",
+        rating: 4.5,
+        numReviews: 14,
+        descritption: "high quality product"
+    },
+    {
+        name: "mayank",
+        slug: "mayank",
+        categories: "Pants",
+        image: '../images/mayank.jpg',
+        price: 78,
+        countInStock: 15,
+        brand: "nike",
+        rating: 4.5,
+        numReviews: 14,
+        descritption: "high quality product"
+    }
+
+]
